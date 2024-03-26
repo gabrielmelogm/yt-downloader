cf830711e68f28c6d83c7f5335977b9822660058
Gabriel Melo <melogoncalvesbiel@gmail.com>
2024-03-25 21:31:57 -0300

    feat: config bin export

--- src/yt.ts
+++ src/yt.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { program } from "commander";
 import { downloadCommand } from "./commands/download.command";
 import { stepForStep } from "./commands/step-for-step.command";
